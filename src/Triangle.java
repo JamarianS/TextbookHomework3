f3b91f25ec47e502b2e8e818960dc6196722e952
Jamarian St.Aimee <jstaimee@ggc.edu>
2023-02-09 19:50:37 -0500

    UPDATED

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Triangle {
+/**
+ * class: Triangle
+ * @author: ITEC 2140 students
+ * @date: February 08, 2023
+ * @version: 1.0
+ * @course: ITEC 2140 - 05 Spring 2023
+ * description: Will ask the user for the size of 3 edges of a triangle then find the perimeter of that triangle,
+ * but only if the sum of 2 sides is greater than the remainder of the other 1.
+ */
+
+
+
+import java.util.Scanner;
+public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an edge of the triangle: ");
+        int length_1 = input.nextInt();
+        System.out.println("Enter the 2nd edge of the triangle: ");
+        int length_2 = input.nextInt();
+        System.out.println("Enter the 3rd edge of the triangle: ");
+        int length_3 = input.nextInt();
+
+        int perimeter = length_1 + length_2 + length_3;
+
+        if (length_1 >= 0 && length_1 + length_2 > length_3) {
+            if (length_2 >= 0 && length_2 + length_3 > length_1) {
+                if (length_3 >= 0 && length_1 + length_3 > length_2) {
+                    System.out.println("The triangle's perimeter is equal to: " + perimeter);
+                }
+            }
+        }
+        else {
+            System.out.println("YOU HAVE AN INVALID INPUT!");
+        }
+    }
 }
--- src/bmi.java
+++ src/bmi.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class bmi {
+/**
+ * class: BMI
+ * @author: ITEC 2140 students
+ * @date: February 08, 2023
+ * @version: 1.0
+ * @course: ITEC 2140 - 05 Spring 2023
+ * description: Will convert pounds to kilograms and then convert inches to meters. With this information you will then
+ * calculate the bmi of an individual.
+ */
+
+import java.util.Scanner;
+public class bmi {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your weight in pounds: ");
+        double weightP = input.nextDouble();
+
+        System.out.println("Enter your height in inches: ");
+        double heightIn = input.nextDouble();
+
+        double weightKg = weightP * 0.4536;
+        double heightM = heightIn * 0.0254;
+
+        double bmi = weightKg / heightM * 0.4536; //* weight in kilos and height in meters
+
+        System.out.println("Your weight is: " + weightKg + " in kilograms ! ");
+        System.out.println("Your height is: " + heightM + " in meters ! ");
+
+        System.out.println("Your BMI is: " + bmi + " ! ");
+
+        if (bmi < 18.5) {
+            System.out.println("You are underweight");
+        }
+        else if (bmi >= 18.5 && bmi < 25.0){
+            System.out.println("You are at a normal weight");
+        }
+        else if (bmi >= 25.0 && bmi < 30.0){
+            System.out.println("You are overweight");
+        }
+        else if( bmi >= 30.0){
+            System.out.println("You are overweight");
+        }
+        else{
+            System.out.println("INVALID INPUT");
+        }
+
+
+
+    }
 }
